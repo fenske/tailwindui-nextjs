cc18c8b96e9888900058b002957c9550adb6164a
Anton Fenske <anton@fenske.xyz>
2020-05-09 22:24:09 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: ['./components/**/*.js', './pages/**/*.js'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/ui'),
+  ],
+}
