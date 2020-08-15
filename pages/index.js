2ad1bb42c68914a86b3cda1d2c252f4399227ebc
Anton Fenske <anton@fenske.xyz>
2020-08-15 07:55:04 +0200

    Bump up dependencies and clean up

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
-import Nav from '../components/nav'
 import Head from 'next/head'
 
 export default function IndexPage() {
   return (
     <div>
       <Head>
-        <title>CoreSkills</title>
+        <title>TailwindUI + NextJS</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
-      <Nav />
       <div className="hero">
-        <h1 className="title">Next.js + Tailwind CSS</h1>
+        <h1 className="text-xl">TailwindUI + NextJS</h1>
       </div>
     </div>
   )
